a2e5045b2a7e2827ec5a94f7be8921b0392c54f4
Caiyi-LI <849845711@qq.com>
2019-01-11 17:04:56 +0800

    加密

--- src/RestfulApiAuth.php
+++ src/RestfulApiAuth.php
@@ -3,13 +3,15 @@ namespace yyc;
 use yyc\Expand\Signature;
 class RestfulApiAuth{
 
-    protected $origin = '';
+    private $origin = [];
 
-    protected $headers = [];
+    private $headers = [];
 
-    protected $isAllowHeaders = ['REQUEST_METHOD', 'CONTENT_TYPE', 'REQUEST_TIME', 'SERVER_NAME', 'QUERY_STRING', 'SERVER_ADDR', 'REMOTE_ADDR', 'SERVER_PORT', 'SERVER_SIGNATURE'];
+    private $isAllowHeaders = ['REQUEST_METHOD', 'CONTENT_TYPE', 'REQUEST_TIME', 'SERVER_NAME', 'QUERY_STRING', 'SERVER_ADDR', 'REMOTE_ADDR', 'SERVER_PORT', 'SERVER_SIGNATURE'];
 
-    private $signature = '';
+    private $isAllowMethods = 'OPTIONS,GET,POST,DELETE,PUT';
+
+    private $isAllowCustomizeHeaders = [];
 
     public function __construct($config = []){
         if (is_array($config)) {
@@ -20,40 +22,57 @@ class RestfulApiAuth{
     }
 
     public function handle(){
-        $this->converHeader();
+        $this->_converHeader();
     }
 
-    private function converHeader(){
+    private function _converHeader(){
         foreach($_SERVER as $serverKey => $item){
             if(in_array($serverKey, $this->isAllowHeaders)){
-                $this->headers[strtolower(str_replace('_', '-', $this->trimSpace($serverKey)))] = $_SERVER[$serverKey];
+                $this->headers[strtolower(str_replace('_', '-', $this->_trimSpace($serverKey)))] = $_SERVER[$serverKey];
             }
             if(substr($serverKey, 0, 5) == 'HTTP_'){
-                $this->headers[strtolower(str_replace('_', '-', $this->trimSpace($serverKey)))] = $item;
+                $this->headers[strtolower(str_replace('_', '-', $this->_trimSpace($serverKey)))] = $item;
             }
         }
         if(strtoupper($this->headers['request-method']) == 'OPTIONS'){
             $params = [];
             $params['host'] = $this->headers['http-host'];
             $signature = new Signature($params);
-            $this->signature = $signature->signature();
-            $this->next();
+            $this->isAllowCustomizeHeaders['_authorization'] = $signature->signature();
+            $this->_next();
+            return;
         }
         echo '后续';
     }
 
-    private function next(){
-        // 指定允许其他域名访问
-        header('Access-Control-Allow-Origin:*');
-        // 响应类型
-        header('Access-Control-Allow-Methods:OPTIONS');
-        // 响应头设置
-        header('Access-Control-Allow-Headers:x-requested-with,content-type');
-        header('Access-Control-Allow-Headers:authorization');
-        header('authorization:authorization');
+    private function _next(){
+        $this->_allowOrigin();
+        $this->_allowMethods();
+        $this->_allowHeaders();
     }
 
-    private function trimSpace($key){
+    private function _allowOrigin(){
+        if(!in_array(str_replace('http://', '', $this->headers['http-origin']), $this->origin)){
+            header('HTTP/1.1 403 Forbidden');
+            return;
+        }
+        header('Access-Control-Allow-Origin:'. $this->headers['http-origin'] .'');
+    }
+
+    private function _allowMethods(){
+        header('Access-Control-Allow-Methods:'. $this->isAllowMethods .'');
+    }
+
+    private function _allowHeaders(){
+        if(is_array($this->isAllowCustomizeHeaders)){
+            foreach($this->isAllowCustomizeHeaders as $headerKey => $customizeHeader){
+                header('Access-Control-Allow-Headers:'. $headerKey .'');
+                header(''. $headerKey .':'. $customizeHeader .'');
+            }
+        }
+    }
+
+    private function _trimSpace($key){
         return preg_replace('/[ |\n\s|\n]/', '', $key);
     }
 }
